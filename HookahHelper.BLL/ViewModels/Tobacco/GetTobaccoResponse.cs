fee90a60cd45577af65fa1f218fc855d943243e2
vlad <vladislavvillya@gmail.com>
2023-07-27 21:36:46 +0300

    added Id to view model

--- HookahHelper.BLL/ViewModels/Tobacco/GetTobaccoResponse.cs
+++ HookahHelper.BLL/ViewModels/Tobacco/GetTobaccoResponse.cs
@@ -4,16 +4,9 @@ namespace HookahHelper.BLL.ViewModels.Tobacco;
 
 public record GetTobaccoResponse
 {
+    public required string Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
-    // public int Sweetness { get; set; }
-    // public int Acidity { get; set; }
-    // public int Spice { get; set; }
-    // public int Freshness { get; set; }
-    // public int Rating { get; set; }
-    // public int Taste { get; set; }
-    // public int Fortress { get; set; }
-    // public int Smokiness { get; set; }
     public string? LineId { get; set; }
     public required string BrandId { get; set; }
     public GetImageResponse Image { get; set; }
