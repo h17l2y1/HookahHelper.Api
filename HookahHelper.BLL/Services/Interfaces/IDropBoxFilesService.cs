0aed97541aac951c97b9c42397d48a4de0967fb0
vlad <vladislavvillya@gmail.com>
2023-07-28 20:15:43 +0300

    test dropbox

---
+++ HookahHelper.BLL/Services/Interfaces/IDropBoxFilesService.cs
@@ -0,0 +1,7 @@
+﻿namespace HookahHelper.BLL.Services.Interfaces;
+
+public interface IDropBoxFilesService
+{
+    Task<byte[]> GetFile(string file);
+    Task WriteFile(string fileName, byte[] content);
+}
