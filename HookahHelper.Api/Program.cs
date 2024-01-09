ba0a8ce6391c9bb82bea16a2a8a33ebe23a67c53
vlad <vladislavvillya@gmail.com>
2024-01-09 23:18:04 +0200

    temp AllowAnyOrigin

--- HookahHelper.Api/Program.cs
+++ HookahHelper.Api/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 builder.Services.AddSwaggerGen(setup =>
 {
     var jwtSecurityScheme = new OpenApiSecurityScheme
